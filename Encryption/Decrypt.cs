27352608d294a7e4b963bb2599b4cbfee6c95d4b
Nakzull <andr25f4@zbc.dk>
2023-03-06 11:46:04 +0100

    Add project files.

---
+++ Encryption/Decrypt.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class Decrypt
+    {
+        public string Decrypter(string input)
+        {
+            byte[] array = Encoding.ASCII.GetBytes(input);
+            string text = "";
+            foreach (byte numbers in array)
+            {
+                var whatever = numbers;
+                whatever--;
+                char character = (char)whatever;
+                text += Char.ConvertFromUtf32(whatever);
+            }
+            return text;
+        }
+    }
+}
---
+++ Encryption/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Encrypt encrypt = new Encrypt();
+            Decrypt decrypt = new Decrypt();
+            Console.WriteLine("What do you wish to encrypt?");
+            string inputEncrypt = Console.ReadLine();
+            Console.WriteLine(encrypt.Encryption(inputEncrypt));
+
+            Console.WriteLine("What do you wish to decrypt?");
+            string inputDecrypt = Console.ReadLine();
+            Console.WriteLine(decrypt.Decrypter(inputDecrypt));
+            Console.ReadKey();
+        }
+    }
+}
